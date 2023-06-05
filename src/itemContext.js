2b2ffade23f94de239aa56d0d90d18cf06f45680
Ashish5692 <ashishyadav5692@gmail.com>
2023-06-05 15:28:46 +0530

    Custom Provider

--- src/itemContext.js
+++ src/itemContext.js
@@ -1,5 +1,19 @@
-import { createContext } from "react";
+import { createContext,useState } from "react";
 
 const itemContext = createContext();
 
-export {itemContext}
+function CustomItemContext({children}){
+    const [total, setTotal] = useState(0);
+    const [item, setItem] = useState(0);
+    return (
+        //CustomProvider making use of default provider here
+        <itemContext.Provider value ={
+            {total,setTotal,item,setItem}
+        }>
+            {children}
+        </itemContext.Provider>
+    )
+}
+
+export {itemContext};
+export default CustomItemContext;
