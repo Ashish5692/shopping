186eb54522b24385b0725eb4a9fb8e85207cc30a
Ashish5692 <ashishyadav5692@gmail.com>
2023-06-05 16:10:27 +0530

    CustomHook

--- src/itemContext.js
+++ src/itemContext.js
@@ -1,19 +1,43 @@
-import { createContext,useState } from "react";
+import { createContext,useState,useContext } from "react";
 
-const itemContext = createContext();
+const itemContext = createContext();  //creation of context
 
+//extracting/consuming the value from this file only
+function useValue(){
+    const value = useContext(itemContext);
+    //as it is customHook it will return some value so it will return value which i am getting from itemContext
+    return value;
+}
+
+//providing the context
 function CustomItemContext({children}){
     const [total, setTotal] = useState(0);
     const [item, setItem] = useState(0);
+
+    //logic of manipulating the state
+    const handleAdd = (price) => {
+        setTotal(total+price);
+        setItem(item+1);
+      };
+    
+      const handleRemove = (price) => {
+        if(total <=0){
+          return;
+        }
+        setTotal((prevState)=> prevState-price);
+        // setTotal(total-price)
+        setItem(item-1);
+      };
+
     return (
-        //CustomProvider making use of default provider here
+        //CustomProvider making use of  default provider here
         <itemContext.Provider value ={
-            {total,setTotal,item,setItem}
+            {total,item,handleAdd,handleRemove}
         }>
             {children}
         </itemContext.Provider>
     )
 }
 
-export {itemContext};
+export {itemContext,useValue};
 export default CustomItemContext;
